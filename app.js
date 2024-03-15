fd1ecc566b82e04ef1a3ac4cbf80261db85e9f49
Vladimir Bogachuk <vovkatom@gmail.com>
2024-03-15 11:43:37 +0200

    +

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import moviesRouter from "./routes/moviesRouter.js";
 import contactsRouter from "./routes/contactsRouter.js";
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/movies", moviesRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,6 +1,10 @@
 import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async(req, res) => {
+    const result = await contactsService.listContacts();
+
+    res.json(result);
+};
 
 export const getOneContact = (req, res) => {};
 

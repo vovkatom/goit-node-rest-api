e6cf7807d55090d01a0b1c5d6e66ecd69bc7f4c5
Vladimir Bogachuk <vovkatom@gmail.com>
2024-03-23 09:23:19 +0200

    news

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { mongoose } from "mongoose";
 
 import contactsRouter from "./routes/contactsRouter.js";
 
@@ -21,6 +22,18 @@ app.use((err, req, res, next) => {
     res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-    console.log("Server is running. Use our API on port: 3000");
-});
+const DB_HOST =
+    "mongodb+srv://vovka:TVUG7JZBN0nYxPT2@cluster0.coevgfu.mongodb.net/my-contacts?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(DB_HOST)
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Server is running. Use our API on port: 3000");
+        });
+    })
+    .catch((error) => {
+        process.exit(1);
+        console.error(error.message);
+    });
+
+//TVUG7JZBN0nYxPT2
